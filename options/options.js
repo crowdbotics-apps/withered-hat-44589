364930ceece0d872a2e840bb0eea68025de08755
Crowdbotics <team@crowdbotics.com>
2023-11-16 13:41:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-hat-44589",
+  url: "https://withered-hat-44589.botics.co",
+  api: "https://withered-hat-44589.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
